63b63a05e4e30143addd5404d55c76cc9e42d85e
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-04-02 01:42:35 +0700

    fix: changed constant

--- lib/vehicleConstant.ts
+++ lib/vehicleConstant.ts
@@ -1,13 +1,13 @@
-enum VehicleType {
-    MOTORCYCLE,
-    CAR,
-    BUS,
-}
-
 enum VehicleSize {
     SMALL = 1,
     MEDIUM = 2,
     LARGE = 5,
+}
+
+enum VehicleType {
+    MOTORCYCLE,
+    CAR,
+    BUS,
 }
 
 export { VehicleType, VehicleSize };
