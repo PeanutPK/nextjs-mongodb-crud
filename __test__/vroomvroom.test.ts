b6c2e7934f73cf7abde32131eab8fc5077516c59
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-04-07 10:04:06 +0700

    test: add test cases for getVehicleSize from string function

--- __test__/vroomvroom.test.ts
+++ __test__/vroomvroom.test.ts
@@ -2,7 +2,7 @@ import Car from "../models/Car";
 import Motorcycle from "../models/Motorcycle";
 import Bus from "../models/Bus";
 import ParkingLot from "../models/ParkingLot";
-import { VehicleSize } from "../lib/vehicleConstant";
+import { VehicleSize, getVehicleSize } from "../lib/vehicleConstant";
 
 describe("ParkingLot", () => {
 	let car: Car;
@@ -11,7 +11,7 @@ describe("ParkingLot", () => {
 	let parkingLot: ParkingLot;
 
 	beforeEach(() => {
-        ParkingLot.resetInstance();
+		ParkingLot.resetInstance();
 		car = new Car("123");
 		motorcycle = new Motorcycle("456");
 		bus = new Bus("789");
@@ -28,24 +28,68 @@ describe("ParkingLot", () => {
 	});
 
 	test("parkVehicle", () => {
-        expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
+		expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
 		expect(parkingLot.parkVehicle(car)).toBe(true);
-        expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(true);
-        expect(parkingLot.findVehicle(motorcycle.getLicensePlate())).toBe(false);
+		expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(true);
+		expect(parkingLot.findVehicle(motorcycle.getLicensePlate())).toBe(
+			false
+		);
 		expect(parkingLot.parkVehicle(motorcycle)).toBe(true);
 		expect(parkingLot.parkVehicle(bus)).toBe(true);
 	});
 
-    test("removeVehicle", () => {
-        expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
-        expect(parkingLot.parkVehicle(car)).toBe(true);
-        expect(parkingLot.parkVehicle(motorcycle)).toBe(true);
-        expect(parkingLot.parkVehicle(bus)).toBe(true);
-        // check if park then remove
-        expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(true);
-        car.clearSpot();
-        expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
+	test("removeVehicle", () => {
+		expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
+		expect(parkingLot.parkVehicle(car)).toBe(true);
+		expect(parkingLot.parkVehicle(motorcycle)).toBe(true);
+		expect(parkingLot.parkVehicle(bus)).toBe(true);
+		// check if park then remove
+		expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(true);
+		car.clearSpot();
+		expect(parkingLot.findVehicle(car.getLicensePlate())).toBe(false);
 
-        expect(parkingLot.parkVehicle(car)).toBe(true);
-    });
+		expect(parkingLot.parkVehicle(car)).toBe(true);
+	});
+});
+
+describe("VehicleSize", () => {
+	let car: Car;
+	let motorcycle: Motorcycle;
+	let bus: Bus;
+
+	beforeEach(() => {
+		car = new Car("123");
+		motorcycle = new Motorcycle("456");
+		bus = new Bus("789");
+	});
+
+	test("stringToVehicleSize", () => {
+		expect(getVehicleSize("MEDIUM")).toBe(VehicleSize.MEDIUM);
+		expect(getVehicleSize("SMALL")).toBe(VehicleSize.SMALL);
+		expect(getVehicleSize("LARGE")).toBe(VehicleSize.LARGE);
+		expect(getVehicleSize("MEDIUM")).toBe(car.getVehicleSize());
+		expect(getVehicleSize("SMALL")).toBe(motorcycle.getVehicleSize());
+		expect(getVehicleSize("LARGE")).toBe(bus.getVehicleSize());
+	});
+
+	test("invalidVehicleSize", () => {
+		expect(() => getVehicleSize("INVALID")).toThrowError(
+			"Invalid vehicle size"
+		);
+	});
+
+	test("getVehicleSize", () => {
+		expect(car.getVehicleSize()).toBe(VehicleSize.MEDIUM);
+		expect(motorcycle.getVehicleSize()).toBe(VehicleSize.SMALL);
+		expect(bus.getVehicleSize()).toBe(VehicleSize.LARGE);
+	});
+
+	test("vehicleTypeSize", () => {
+		expect(car.getVehicleSize()).toBe(VehicleSize.MEDIUM);
+		expect(car.getSpotNeeded()).toBe(1);
+		expect(motorcycle.getVehicleSize()).toBe(VehicleSize.SMALL);
+		expect(motorcycle.getSpotNeeded()).toBe(1);
+		expect(bus.getVehicleSize()).toBe(VehicleSize.LARGE);
+		expect(bus.getSpotNeeded()).toBe(5);
+	});
 });
