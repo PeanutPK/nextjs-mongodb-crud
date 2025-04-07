92f672001c779a55baa27c3adcced66d97aaca06
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-04-07 09:59:27 +0700

    feat: string to vehicle size converter

--- lib/vehicleConstant.ts
+++ lib/vehicleConstant.ts
@@ -5,4 +5,17 @@ enum VehicleSize {
     LARGE="LARGE",
 }
 
+export function getVehicleSize(size: string): VehicleSize {
+    switch (size) {
+        case "SMALL":
+            return VehicleSize.SMALL;
+        case "MEDIUM":
+            return VehicleSize.MEDIUM;
+        case "LARGE":
+            return VehicleSize.LARGE;
+        default:
+            throw new Error("Invalid vehicle size");
+    }
+}
+
 export { VehicleSize };
